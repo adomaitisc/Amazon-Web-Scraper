7353ffbbbd604393c8ac016f0dd9ebb1409c743e
Cauã Adomaitis <caadomaitis@gmail.com>
2022-03-03 11:14:11 -0500

    scraper has basic functions working

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {		
-		Scraper product = new Scraper("https://www.amazon.com/at-at-Ultimate-Collector-Building-Pieces/dp/B09MV3H7WX/ref=pb_allspark_dp_sims_pao_desktop_session_based_5/140-3123829-3494248?pd_rd_w=VswQ5&pf_rd_p=e896123b-6614-49c5-873e-d532e726c2f0&pf_rd_r=44T0AXNWQRQDKB76PMJB&pd_rd_r=375faa50-99aa-4270-9c3f-18fa5174f2af&pd_rd_wg=ZlTvZ&pd_rd_i=B09MV3H7WX&psc=1");
-		System.out.println(product.getTitle());
+		testScraper();
+	}
+	
+	public static void testScraper() {
+		String productURL = "https://www.amazon.com/LEGO-Ultimate-Millennium-Falcon-Building/dp/B075SDMMMV";
+		
+		Scraper Puzzle = new Scraper(productURL);
+		
+		System.out.println(Puzzle.NAME);
+		System.out.println(Puzzle.ID);
+		System.out.println(Puzzle.PRICE);
+		System.out.println(Puzzle.AVAILABLE);
+		
+		System.out.println(Puzzle);
+		
 	}
 
 }
--- src/Scraper.java
+++ src/Scraper.java
@@ -1,88 +1,62 @@
-import java.io.IOException;
 
+import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class Scraper {
 	
-	private boolean AVAILABLE;
-	private String URL;
-	private String ID;
-	private String NAME;
-	private String PRICE;
 	private Document DOC;
+	public boolean AVAILABLE;
+	public String URL;
+	public String ID;
+	public String NAME;
+	public String PRICE;
 
 	public Scraper(String productURL) {
 		this.URL = productURL;
-		System.out.println("Scraper Running");
 		
 		try {
 			this.DOC = Jsoup.connect(productURL)
-//					.header("Host", "www.amazon.com")
-					.header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15")
-//				    .header("Accept-Language", "en-US,en;q=0.9")
-//				    .header("Accept-Encoding", "gzip, deflate, br")
-//				    .header("Connection", "keep-alive")
-					.get();
+				.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36")
+				.get();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		getAllData();
 	}
 	
-	//See if price is on product page
-	public boolean isAvailable() {
-		return true;
-	}
-	
-	//See if price is on product page
-	public String getTitle() {
-		return DOC.title();
-	}
-	
-	//get an id for the product
-	private String getProductId() {
-		return "ID";
-	}
-	
-	private String getProductName() {
-		//get name from document
-		return "NAME";
-	}
-	
-	private String getProductPrice() {
-		//get price from document
-		return "PRICE";
-	}
-	
-	private String getProductBuyingOptionURL() {
-		//get buying option URL from document
-		return "B_O_URL";
-	}
-	
-	private String getProductBuyingOptionPrice() {
-		//get buying option price from document
-		return "B_O_PRICE";
-	}
-	
-	
-	private void initializeScraper() {
-		this.AVAILABLE = isAvailable();
-		this.ID = getProductId();
-		this.NAME = getProductName();
-		if(this.AVAILABLE) {
-			this.PRICE = getProductPrice();
-		}
-		else {
-			this.URL = getProductBuyingOptionURL();
-			this.PRICE = getProductBuyingOptionPrice();
+	private void isAvailable() {
+		String buy = DOC.select("div#buyNow").first().text();
+		if(buy.length() > 3) {
+			this.AVAILABLE = true;
+		} else {
+			this.AVAILABLE = false;
 		}
 	}
 	
-	public String getProductData() {
-		initializeScraper();
-		//build json object and return as string
-		return "Product Data";
+	private void getProductId() {
+		String[] ids = this.URL.split("/", 0);
+		this.ID = ids[5];
+	}
+	
+	private void getProductName() {
+		this.NAME = DOC.select("span.a-size-large").first().text();
+	}
+	
+	private void getProductPrice() {
+		this.PRICE = DOC.select("span.a-offscreen").first().text();
+	}
+	
+	private void getAllData() {
+		isAvailable();
+		getProductId();
+		getProductName();
+		getProductPrice();
+	}
+	
+	public String toString() {
+		if(AVAILABLE) return "This item is available";
+		return "This item is not available";
 	}
 }
